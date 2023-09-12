be6fddc520b4f3af257b9d5809e3bf4f9f7cf259
felipricardo <felipebarbosaok@gmail.com>
2023-09-12 17:01:08 -0300

    up

--- Program.cs
+++ Program.cs
@@ -81,6 +81,16 @@ public class ApplicationDbContext : DbContext {
 
     public DbSet<Product> Products { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder builder) 
+    {
+        builder.Entity<Product>()
+            .Property(p => p.Description).HasMaxLength(500).IsRequired(false);
+        builder.Entity<Product>()
+            .Property(p => p.Name).HasMaxLength(120).IsRequired();
+        builder.Entity<Product>()
+            .Property(p => p.Code).HasMaxLength(20).IsRequired();
+    }
+
     // Configurando o contexto do banco de dados
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlServer("Server=localhost;Database=Products;User Id=sa;Password=@Sql2023;MultipleActiveResultSets=true;Encrypt=YES;TrustServerCertificate=YES");
