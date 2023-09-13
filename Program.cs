0185fc650bd182eade5e6406d82c2d2252c9d988
felipricardo <felipebarbosaok@gmail.com>
2023-09-13 09:39:26 -0300

    up

--- Program.cs
+++ Program.cs
@@ -78,12 +78,18 @@ public static class ProductRepository {
     }
 }
 
+public class Category {
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
 // Classe de modelo de produto
 public class Product {
     public int Id { get; set; }
     public string Code { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public Category Category { get; set; }
 }
 
 // Classe de contexto de banco de dados
