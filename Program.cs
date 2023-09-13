eb2e7618cef33effa740f56a9253465a52ccfb6d
felipricardo <felipebarbosaok@gmail.com>
2023-09-13 09:54:38 -0300

    up

--- Program.cs
+++ Program.cs
@@ -89,6 +89,7 @@ public class Product {
     public string Code { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public int CategoryId { get; set; }
     public Category Category { get; set; }
 }
 
